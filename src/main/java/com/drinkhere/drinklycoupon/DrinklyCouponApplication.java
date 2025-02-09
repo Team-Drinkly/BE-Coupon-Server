82f2d7846f6979f8283ad32cf4ebd6288e4a640e
안정후 <austinan@naver.com>
2025-02-09 21:23:16 +0900

    chore: drinkly coupon settings

---
+++ src/main/java/com/drinkhere/drinklycoupon/DrinklyCouponApplication.java
@@ -0,0 +1,13 @@
+package com.drinkhere.drinklycoupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrinklyCouponApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DrinklyCouponApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/drinkhere/drinklycoupon/DrinklyCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drinkhere.drinklycoupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrinklyCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
