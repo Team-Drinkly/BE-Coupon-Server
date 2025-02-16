0bbbb6df19cfaaa004888644a9a4d1a0eef04f5d
안정후 <austinan@naver.com>
2025-02-16 14:11:42 +0900

    feat: application settings

--- src/main/java/com/drinkhere/drinklycoupon/DrinklyCouponApplication.java
+++ src/main/java/com/drinkhere/drinklycoupon/DrinklyCouponApplication.java
@@ -2,8 +2,14 @@ package com.drinkhere.drinklycoupon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableJpaAuditing
+@EnableKafka
 public class DrinklyCouponApplication {
 
     public static void main(String[] args) {
