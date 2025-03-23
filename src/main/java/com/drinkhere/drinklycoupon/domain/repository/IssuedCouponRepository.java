2da00da04d54f7b9b5806a8003a23ed18aa912ea
안정후 <austinan@naver.com>
2025-03-23 14:31:40 +0900

    feat: coupon repository & dto

---
+++ src/main/java/com/drinkhere/drinklycoupon/domain/repository/IssuedCouponRepository.java
@@ -0,0 +1,17 @@
+package com.drinkhere.drinklycoupon.domain.repository;
+
+import com.drinkhere.drinklycoupon.domain.entity.IssuedCoupon;
+import com.drinkhere.drinklycoupon.dto.CouponStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IssuedCouponRepository extends JpaRepository<IssuedCoupon, Long> {
+
+    Optional<IssuedCoupon> findByMemberIdAndCouponIdAndStatus(Long memberId, Long couponId, CouponStatus status);
+
+    List<IssuedCoupon> findByMemberIdAndStatus(Long memberId, CouponStatus status);
+
+    boolean existsByMemberIdAndCouponId(Long memberId, Long couponId);
+}
